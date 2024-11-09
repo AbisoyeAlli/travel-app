714bf788be5bf5c66fed1cb5b34d48d25147cd6b
Abisoye Alli Balogun <abisoyeallibalogun@Abisoyes-MacBook-Pro.local>
2024-11-09 23:06:50 +0000

    chore:project setup

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+   
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text style={{
+        fontFamily: 'outfit-bold',
+        fontSize: 30,
+      }}>Travel App</Text>
+    </View>
+  );
+}
